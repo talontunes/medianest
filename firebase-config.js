978102081be697272904e7d1ab1643c56cddc548
cmingh <huhin18@gmail.com>
2026-06-04 09:42:08 -0400

    sixth iteration, added firebase config file and updated gitignore to ignore node modules and firebase config file

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBLla3uTvBOTnHskT_4IuAY33MAjBFei7k",
+  authDomain: "medianest-cf71b.firebaseapp.com",
+  projectId: "medianest-cf71b",
+  storageBucket: "medianest-cf71b.firebasestorage.app",
+  messagingSenderId: "117838127157",
+  appId: "1:117838127157:web:f528a1eefcec0ca116517c",
+  measurementId: "G-LP4CXC2KYW"
+};
